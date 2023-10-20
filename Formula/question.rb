4432ee0c2818dc0e6d77337fdced14319c938a03
GitHub Action <action@github.com>
2023-10-20 02:10:50 +0000

    Update question formula to version

--- Formula/question.rb
+++ Formula/question.rb
@@ -1,8 +1,8 @@
 class Question < Formula
   desc "CLI to ask questions to ChatGPT"
   homepage "https://github.com/cdemers/question"
-  url "https://github.com/cdemers/question/releases/download/0.5.2/question.tgz"
-  sha256 "put_the_sha256_hash_here"
+  url "https://github.com/cdemers/question/releases/download/0.6.0/question.tgz"
+  sha256 "dfa97c13d7673a9db1c3c0c9ffd0f768f8e04cca32311c968684d01ffd1d11ee"
 
   depends_on "python@3.9"
 
